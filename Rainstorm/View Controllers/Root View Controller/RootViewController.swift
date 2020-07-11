9cfec0d183132ac515dcaf7e0d2d34f8995e96bb
Olajuwon Balogun <olajuwon@softcom.ng>
2020-07-11 14:33:46 +0100

    project setup

---
+++ Rainstorm/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  Rainstorm
+//
+//  Created by OLAJUWON BALOGUN on 11/07/2020.
+//  Copyright © 2020 OLAJUWON BALOGUN. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    
+    //MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
