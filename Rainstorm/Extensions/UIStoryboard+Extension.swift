945db707c9cbab9822ccc23a49bb2b3369e731de
Olajuwon Balogun <olajuwon@softcom.ng>
2020-07-11 15:12:36 +0100

    setup childView controllers in root view controller

---
+++ Rainstorm/Extensions/UIStoryboard+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIStoryboard+Extension.swift
+//  Rainstorm
+//
+//  Created by OLAJUWON BALOGUN on 11/07/2020.
+//  Copyright © 2020 OLAJUWON BALOGUN. All rights reserved.
+//
+
+import Foundation
